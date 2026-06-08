bb8efe48254853d67b4f057e1e1fa1df4f4e6eee
meetpv287 <meetpvaghasiya287@gmail.com>
2026-06-08 16:13:25 +0530

    Remove Day 8 and Day 9 folders

--- Day 8/script.js
+++
@@ -1,9 +0,0 @@
-let exploreBtn = document.getElementById("exploreBtn");
-exploreBtn.addEventListener("click",()=>{
-    alert("Welcome To WanderRoute ");
-});
-
-let bookBtn = document.getElementById("bookBtn");
-bookBtn.addEventListener("click",()=>{
-    alert("Your Trip Has Been Booked ");
-});
